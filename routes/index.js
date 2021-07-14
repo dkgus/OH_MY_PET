411e0f46bf06f22eea12fa1b96fff8626b6e4057
dkgus12dkgus <dkgus12dkgus@naver.com>
2021-07-14 18:10:30 +0900

    UPDATE mainRouter/View

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User");
+const Community = require("../models/Community");
+const Notice = require("../models/Notice");
 
-router.get("/", async (req, res, next) => {
-	const data = {
-        addCss : ['main'],
-        addScript : ['main'],
-    }
-    return res.render("main/index", data);
 
+
+
+router.get("/", async function(req, res){
+    const posts = await Community.find({});
+    const notices = await Notice.find({});
+    const user = await User.find({})
+
+   
+    res.render("main/index",{ posts,notices,user });
 });
+
 
 module.exports = router;
