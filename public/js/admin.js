fc95390d4aa9436f494a267225897f839c11f10c
정유 <98jungyu@gmail.com>
2021-07-30 22:17:37 +0900

    수정 및 추가

--- public/js/admin.js
+++ public/js/admin.js
@@ -9,18 +9,3 @@ for (let i=0; i<list.length; i++){
         list[i].className = 'list active';
     }
 }
-
-//
-$(document).ready(function(){
-
-    $('ul.tabs li').click(function(){
-      var tab_id = $(this).attr('data-tab');
-  
-      $('ul.tabs li').removeClass('current');
-      $('.tab-content').removeClass('current');
-  
-      $(this).addClass('current');
-      $("#"+tab_id).addClass('current');
-    })
-  })
-  
