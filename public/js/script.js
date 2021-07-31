bd32891563383e7e5ce4bf160b358d1a4d67f168
hyeonbin-jung <reno123@nate.com>
2021-07-31 21:51:25 +0900

    모먼트 삭제

--- public/js/script.js
+++ public/js/script.js
@@ -7,16 +7,16 @@ const sideBtn = {
       $(".sideList").removeClass("on");
     }
   };
-  
+
   $(() => {
     $(".sideWrap").click(function() { //열려있으면 닫기
       if($(".sideList").hasClass("on")) {
         sideBtn.close();
-      } else { 
+      } else {
         sideBtn.open();
       }
     });
-  
+
     $(".sideWrap").mouseleave(function() {
         sideBtn.close();
     });
